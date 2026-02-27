791a4a24ce56c53303fd1e1ff3d518dce2f2f9bf
leticiagabriell <leticiagabriellyleticia0708@gmail.com>
2026-02-27 16:10:20 -0300

    Add files via upload

---
+++ distancia.py
@@ -0,0 +1,8 @@
+distancia=float(input(" Qual a distância percorrida (em km): "))
+
+gasolina=float(input(" Quanto de gasolina vc gastou (em litros): "))
+
+consumo= distancia / gasolina
+
+print("o consumo medio é:",consumo)
+
