293fc924c9415866efef5b00fb08b894a6fa23fc
leticiagabriell <leticiagabriellyleticia0708@gmail.com>
2026-03-04 11:15:09 -0300

    Add files via upload

---
+++ UC-algoritimo-04-03/compra.py
@@ -0,0 +1,6 @@
+lista_c= ["ovos","leite","farinha","arroz","frango"]
+adicionar=input("adicione um item a lista:")
+lista_c.append(adicionar)
+print(lista_c)
+
+
---
+++ UC-algoritimo-04-03/lista.py
@@ -0,0 +1,21 @@
+nomes=["Ana","Bruno","caio"]
+print(nomes)
+
+dados = ["Carol",0,1.70,True]
+print(dados)
+print(type(dados [0]))
+print(type(dados [1]))
+print(type(dados [2]))
+print(type(dados [3]))
+
+lista = ["cachorro","gato"]
+print("original : ",lista)
+
+lista.append("coelho")
+print("atualizado:",lista)
+
+lista . insert(1,"Grilo")
+print("atualizando:",lista)
+
+lista. extend(["Macaco","Ovelha"])
+print("Lista final",lista)
