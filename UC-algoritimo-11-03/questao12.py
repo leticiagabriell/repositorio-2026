59aff8d59daa06e2fec23aae232d51cd70b4d1ec
leticiagabriell <leticiagabriellyleticia0708@gmail.com>
2026-03-11 15:06:50 -0300

    Update deposit input handling in questao12.py

--- UC-algoritimo-11-03/questao12.py
+++ UC-algoritimo-11-03/questao12.py
@@ -1,1 +1,10 @@
-numero = int(input("digite um numero:"))
+numero=int(input("digite o valor do deposito:"))
+
+while numero != 0 :
+    if numero <0:
+       numero=int(input("digite o valor do deposito:"))
+       continue
+
+    deposito+=numero
+    numero=int(input("digite o valor do deposito:"))
+print("valor total dos depositos]",deposito)
