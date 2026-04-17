a1ca3fb0fd4a7379ed240dcb2c2798b55535d3e4
leticiagabriell <leticiagabriellyleticia0708@gmail.com>
2026-04-17 17:40:35 -0300

    Add total friends count check for even or odd

--- UC-algortimo17-04/questao5.py
+++ UC-algortimo17-04/questao5.py
@@ -1,1 +1,8 @@
 amigos = ["Alice", "Beto", "Caio", "Duda", "Eva"]
+total = len(amigos)
+
+if total % 2 == 0:
+    print(f"O total de {total} amigos é PAR.")
+else:
+    print(f"O total de {total} amigos é ÍMPAR.")
+
