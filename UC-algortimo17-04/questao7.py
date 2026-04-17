c7877790e0b57ff24ea90e2ce3730a65b30004ec
leticiagabriell <leticiagabriellyleticia0708@gmail.com>
2026-04-17 17:38:20 -0300

    Update questao7.py

--- UC-algortimo17-04/questao7.py
+++ UC-algortimo17-04/questao7.py
@@ -1,5 +1,8 @@
-produtos = []
-total = 0 
-produto = float(input("valor do prouto:"))
-for  produto  % 2 == 0:
-    produto+=total 
+vendas = [10, 15, 20, 25, 30, 35, 40]
+soma = 0
+
+for valor in vendas:
+    if valor % 2 == 0:
+        soma += valor
+
+print(f"A soma dos valores pares é: {soma}")
